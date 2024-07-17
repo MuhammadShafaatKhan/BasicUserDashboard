f9352e8addbb32f9df0b9d227fb2c03995af7343
Muhammad Shafaat Khan <102707063+MuhammadShafaatKhan13@users.noreply.github.com>
2024-07-17 13:00:13 +0930

    feat: using router to show signIn and signUp pages

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,21 @@
-
+// TODO: for backend use json server: https://dev.to/jyeett/the-basic-necessities-of-a-mock-user-authentication-with-json-server-auth-2onj
+// once complete dashboard working, move backend to strapi
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import SignUp from './pages/sign-up'
+import SignIn from "./pages/sign-in";
 
 function App() {
   
 
   return (
     <>
-      <SignUp/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/sign-in/" element={<SignIn />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
